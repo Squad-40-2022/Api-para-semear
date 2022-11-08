e854b8e8a888de283df28cad13f6732ea59e8429
Angélica Flores <99211551+Angelicadf97@users.noreply.github.com>
2022-11-08 16:56:47 -0300

    Initial commit

---
+++ src/main/java/br/org/com/parasemear/controller/package-info.java
@@ -0,0 +1,1 @@
+package br.org.com.parasemear.controller;
---
+++ src/test/java/br/org/com/parasemear/ApiParaSemearApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.com.parasemear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParaSemearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
